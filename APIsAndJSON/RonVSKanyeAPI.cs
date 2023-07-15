69d5398cc8e4f76db731d053d941b3643c480d20
Michael Berg <mberg12@bu.edu>
2023-07-15 16:45:19 -0400

    first exercise

--- APIsAndJSON/RonVSKanyeAPI.cs
+++ APIsAndJSON/RonVSKanyeAPI.cs
@@ -2,11 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Nodes;
 using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
 
 namespace APIsAndJSON
 {
     internal class RonVSKanyeAPI
     {
+        public static void Convo()
+        {
+
+
+
+            var client = new HttpClient();
+
+
+
+            for (int i = 0; i < 6; i++)
+            {
+
+                var kanyeUrl = "https://api.kanye.rest/";
+
+                var kRepsone = client.GetStringAsync(kanyeUrl).Result;
+                var kObject = JsonNode.Parse(kRepsone);
+                Console.WriteLine($"Kayne:{kObject["quote"]}");
+                Console.WriteLine();
+
+
+                var ronUrl = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+                var rResponse = client.GetStringAsync(ronUrl).Result;
+                JsonNode rObject = JsonNode.Parse(rResponse);
+                Console.WriteLine($"Ron:{rObject[0]}");
+                Console.WriteLine();
+
+            }
+            
+        }
+
     }
 }
